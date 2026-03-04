1928e810c44a7f9ddcb991688d452025836f1037
Akibayashi <egormalakhov05@mail.ru>
2026-03-04 18:14:35 +0300

    Initial beautiful Astro project

---
+++ src/lib/sanity.js
@@ -0,0 +1,16 @@
+// src/lib/sanity.js
+import {createClient} from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const sanityClient = createClient({
+  projectId: 'ipigbfxk', // Твой Project ID уже вставлен
+  dataset: 'opera',
+  apiVersion: '2026-03-03', // Твой API Version
+  useCdn: true,
+});
+
+const builder = imageUrlBuilder(sanityClient);
+
+export function urlFor(source) {
+  return builder.image(source);
+}

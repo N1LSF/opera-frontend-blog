40fe3dcc20edc05e0ed8d04afeefcb343bac2783
Akibayashi <egormalakhov05@mail.ru>
2026-03-04 20:01:57 +0300

    Enable prerender = true to fix Sanity lag

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -5,8 +5,8 @@ import imageUrlBuilder from '@sanity/image-url';
 export const sanityClient = createClient({
   projectId: 'ipigbfxk', // Твой Project ID уже вставлен
   dataset: 'opera',
-  apiVersion: '2026-03-03', // Твой API Version
-  useCdn: false,
+  apiVersion: '2026-03-04', // Твой API Version
+  useCdn: true,
 });
 
 const builder = imageUrlBuilder(sanityClient);
